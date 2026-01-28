048c66d9dee707af3e219d52a7c9aed2d5db3982
Nathanwack <nathan.wack1@gmail.com>
2026-01-28 12:28:32 +0100

    Navbar modifié.

--- _partial/navbar.php
+++ _partial/navbar.php
@@ -1,5 +1,24 @@
-<nav>
-    <img src="assets/Logo.png" alt="">
-    <a href="pages/reservation.php">Réservation</a>
-    <a href="pages/salle.php">Salles</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="index.php">
+      <img src="assets/Logo.png" alt="Logo" height="40">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mainNavbar">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="pages/reservation.php">Réservation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pages/salle.php">Salles</a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
 </nav>
