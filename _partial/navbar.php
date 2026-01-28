6738739dc17549f470ef29019304c19f0b31d90c
Nathanwack <nathan.wack1@gmail.com>
2026-01-28 12:32:11 +0100

    Fix chemin absolu

--- _partial/navbar.php
+++ _partial/navbar.php
@@ -12,10 +12,10 @@
     <div class="collapse navbar-collapse" id="mainNavbar">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="pages/reservation.php">Réservation</a>
+          <a class="nav-link" href="/pages/reservation.php">Réservation</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="pages/salle.php">Salles</a>
+          <a class="nav-link" href="/pages/salle.php">Salles</a>
         </li>
       </ul>
     </div>
